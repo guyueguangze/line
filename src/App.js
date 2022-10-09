a4e1b20a42c60c0fb3e83e7e8ecf0dbcc90b5782
胡光泽 <2581504687@qq.com>
2022-10-09 12:01:08 +0800

    再次部署

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch, Redirect, Router } from 'react-router-dom'
+import history from './utils/history'
 
-function App() {
+import Layout from './page/Layout'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="root">
+      <Router history={history}>
+        <Layout></Layout>
+      </Router>
     </div>
-  );
+  )
 }
-
-export default App;
